afa2b7c2b24a266847bb50a69858d6ad9c569a56
Uhm-why <dustin.hacker0@icloud.com>
2025-08-03 16:08:03 -0600

    Removed unnecessary spacing.

--- fileio/fileio.go
+++ fileio/fileio.go
@@ -16,13 +16,9 @@ func ChunkFile(args []string) {
 	fi.Size, err = chunk.GetFileSize(fname)
 
 	if err != nil {
-
 		fmt.Printf("Error: %s.\n", err)
-
 	} else {
-
 		fmt.Printf("File Size: %v bits\n", fi.Size)
-
 	}
 
 	fi.ChunkSize = chunk.SelectChunkSize(fi.Size)
@@ -31,21 +27,14 @@ func ChunkFile(args []string) {
 	fi.NumberOfChunks, err = chunk.SelectNumberOfChunks(fi.Size, fi.ChunkSize)
 
 	if err != nil {
-
 		fmt.Printf("Error: %v\n", err)
-
 	} else {
-
 		if fi.NumberOfChunks > 1 {
-
 			fi.HasChunks = true
 			fmt.Printf("File will be divided into %v chunks\n", fi.NumberOfChunks)
-
 		} else {
-
 			fi.HasChunks = false
 			fmt.Printf("File will be sent as one chunk.\n")
-
 		}
 
 	}
@@ -53,13 +42,9 @@ func ChunkFile(args []string) {
 	fi.Hash, err = HashFile(fi.Name)
 
 	if err != nil {
-
 		fmt.Printf("Error: %s.", err)
-
 	} else {
-
 		fmt.Printf("File SHA256 Hash: %s\n", fi.Hash)
-
 	}
 
 }
