78836c3c81056c28a6bdf263a3ccb1d65a619b2f
Uhm-why <dustin.hacker0@icloud.com>
2025-08-03 22:23:13 -0600

    Moved GetFileInfo() to info.go

--- fileio/fileio.go
+++ fileio/fileio.go
@@ -3,52 +3,10 @@ package fileio
 import (
 	"fmt"
 	"strings"
-
-	"github.com/Uhm-why/gusher/chunk"
 )
-
-func GetFileInfo(fname string) FileInfo {
-	var err error
-
-	var fi FileInfo = FileInfo{Name: fname}
-	fi.Size, err = chunk.GetFileSize(fname)
-
-	if err != nil {
-		fmt.Printf("Error: %s.\n", err)
-	} else {
-		fmt.Printf("File Size: %v bits\n", fi.Size)
-	}
-
-	fi.ChunkSize = chunk.SelectChunkSize(fi.Size)
-	fmt.Printf("Chunk Size: %v\n", fi.ChunkSize)
-
-	fi.NumberOfChunks, err = chunk.SelectNumberOfChunks(fi.Size, fi.ChunkSize)
-
-	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-	} else {
-		if fi.NumberOfChunks > 1 {
-			fi.HasChunks = true
-			fmt.Printf("File will be divided into %v chunks\n", fi.NumberOfChunks)
-		} else {
-			fi.HasChunks = false
-			fmt.Printf("File will be sent as one chunk.\n")
-		}
-
-	}
-
-	fi.Hash, err = HashFile(fi.Name)
-
-	if err != nil {
-		fmt.Printf("Error: %s.", err)
-	} else {
-		fmt.Printf("File SHA256 Hash: %s\n", fi.Hash)
-	}
-
-	return fi
-}
 
 func ChunkFile(args []string) {
 	fi := GetFileInfo(strings.Join(args, " "))
+	fmt.Printf("TODO: Implement File Chunking Logic... %v\n", fi.Name)
 
 }
